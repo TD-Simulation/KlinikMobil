c1d7eec95a067a5fe44bb97e175bdc125bdbff47
carly.baja <cbaja@hotmail.com>
2016-05-21 11:46:04 -0500

    Login Enterprise
    Login Enterprise

---
+++ app/src/main/java/com/example/carlybaja/klinikmobil/activities/LoginEnterpriseActivity.java
@@ -0,0 +1,35 @@
+package com.example.carlybaja.klinikmobil.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.carlybaja.klinikmobil.R;
+
+public class LoginEnterpriseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_enterprise);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    //public void onLogEntreprise(View v){
+      //  Intent j = new Intent(LoginEnterpriseActivity.this,);
+        //startActivity(j);
+    //}
+
+    //public void onKreyeEntreprise(View v){
+        //  Intent j = new Intent(LoginEnterpriseActivity.this,);
+        //startActivity(j);
+        //}
+
+}
