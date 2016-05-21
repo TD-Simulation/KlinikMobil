1074f5071d4767fa125819c215cd65f44f91b6bc
Djason irvinton SYLVAINCE <djasoirvinton.sylvaince@esih.edu>
2016-05-21 13:55:13 -0400

    ajout des conditions d'entree dans le systeme:
    fichier LoginGenActivity .

--- app/src/main/java/com/example/carlybaja/klinikmobil/activities/LoginGenActivity.java
+++ app/src/main/java/com/example/carlybaja/klinikmobil/activities/LoginGenActivity.java
@@ -1,16 +1,24 @@
 package com.example.carlybaja.klinikmobil.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.carlybaja.klinikmobil.ListFanmAnsentActivityEnt;
 import com.example.carlybaja.klinikmobil.R;
 
 public class LoginGenActivity extends AppCompatActivity {
 
+    private String nomUserE;
+    private String passuserE;
+    EditText editUser;
+    EditText editPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +27,41 @@ public class LoginGenActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        editUser = (EditText) findViewById(R.id.nomUser);
+        editPass = (EditText) findViewById(R.id.passUser);
+    }
+//on click boutton konekt
+    public void onKonektGen (View v){
+        nomUserE = editUser.getText().toString();
+        passuserE = editPass.getText().toString();
+        if(nomUserE.equals("") && passuserE.equals("")){
+            Toast.makeText(getApplicationContext(),"chanp ou yo vid", Toast.LENGTH_LONG).show();
+        }else if(nomUserE.equals("entre") && passuserE.equals("entre")){
+            Toast.makeText(LoginGenActivity.this, "Bonjour " + nomUserE + " !", Toast.LENGTH_LONG).show();
+            Intent KonektGen= new Intent(LoginGenActivity.this, ListFanmAnsentActivityEnt.class);
+            startActivity(KonektGen);
+        }else{
+            Toast.makeText(getApplicationContext(),"nou pa gen nom sa nan systeme nan", Toast.LENGTH_LONG).show();
+        }
 
     }
+
+//on click boutton kreye kont
+public void onKreateK (View v){
+    Intent KreateK= new Intent(LoginGenActivity.this, ed.class);
+    startActivity(KreateK);
+}
+
+//on click boutton oublier mot de pass
+    public void onOubliPass (View v){
+        nomUserE = editUser.getText().toString();
+        if(nomUserE.equals("")){
+            Toast.makeText(getApplicationContext(),"antre nom itilizate ou nan chanp an", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(LoginGenActivity.this,  nomUserE +" ,nap send ou yon mesaj nan yon ti moman !", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 
 }
