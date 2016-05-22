4561399d53eca382f550c015c2056c7c8c0bd688
carly.baja <cbaja@hotmail.com>
2016-05-22 07:23:40 -0500

    update login entreprise
    update login entreprise

--- app/src/main/java/com/example/carlybaja/klinikmobil/activities/LoginGenActivity.java
+++ app/src/main/java/com/example/carlybaja/klinikmobil/activities/LoginGenActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.carlybaja.klinikmobil.LisTimounActivityEnt;
 import com.example.carlybaja.klinikmobil.ListFanmAnsentActivityEnt;
 import com.example.carlybaja.klinikmobil.R;
 
@@ -39,8 +38,8 @@ public class LoginGenActivity extends AppCompatActivity {
             startActivity(konektGen);
         }else if(inputUser.equals("timoun") && inputPass.equals("4321")){
             Toast.makeText(getApplicationContext(),"information timoun yo",Toast.LENGTH_LONG).show();
-            Intent konektGen = new Intent(LoginGenActivity.this, LisTimounActivityEnt.class);
-            startActivity(konektGen);
+           // Intent konektGen = new Intent(LoginGenActivity.this, ListFanmAnsentActivityEnt.class);
+           // startActivity(konektGen);
         }else{
             Toast.makeText(getApplicationContext(),"sisteme nan pa rekonet information sa",Toast.LENGTH_LONG).show();
         }
@@ -61,7 +60,7 @@ public class LoginGenActivity extends AppCompatActivity {
     }
 
     public void onKreye(View v){
-        Toast.makeText(getApplicationContext(),"kontakte yon administrate pou plis infomasyon",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"",Toast.LENGTH_LONG).show();
     }
 
 }
