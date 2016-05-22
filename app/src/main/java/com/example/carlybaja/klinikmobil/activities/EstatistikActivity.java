fa56ada6b4779e88a1e426c7af7abe94dc6f5916
carly.baja <cbaja@hotmail.com>
2016-05-21 19:24:30 -0500

    Update
    Update

--- app/src/main/java/com/example/carlybaja/klinikmobil/activities/EstatistikActivity.java
+++ app/src/main/java/com/example/carlybaja/klinikmobil/activities/EstatistikActivity.java
@@ -17,6 +17,7 @@ public class EstatistikActivity extends AppCompatActivity {
         setContentView(R.layout.activity_estatistik);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
