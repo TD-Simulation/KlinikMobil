0baa0b81a6cb108c27da1c3aa34ecd3151cc67bf
Carly BAJA <cbaja@hotmail.com>
2016-05-22 00:49:31 -0400

    UpChange

--- app/src/main/java/com/example/carlybaja/klinikmobil/activities/LoginGenActivity.java
+++ app/src/main/java/com/example/carlybaja/klinikmobil/activities/LoginGenActivity.java
@@ -1,5 +1,6 @@
 package com.example.carlybaja.klinikmobil.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.carlybaja.klinikmobil.ListFanmAnsentActivityEnt;
 import com.example.carlybaja.klinikmobil.R;
 
 public class LoginGenActivity extends AppCompatActivity {
@@ -18,8 +20,11 @@ public class LoginGenActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-
+    public void onKonekteFanm(View v){
+        Intent i = new Intent(LoginGenActivity.this, ListFanmAnsentActivityEnt.class);
+        startActivity(i);
     }
 
 }
